21d55da28a16326426fc8d8349c8ecc5077a3f6a
BMO <bmo@skypress.io>
2020-10-28 09:28:49 -0500

    wip: update

--- data/src/core/core.module.ts
+++ data/src/core/core.module.ts
@@ -1,6 +1,7 @@
 import { Module } from '@nestjs/common';
 import { DbService, QueryService, StringService } from '@/core/services';
 
+// A new Core Module
 @Module({
   providers: [DbService, QueryService, StringService],
   exports: [DbService, QueryService, StringService],
